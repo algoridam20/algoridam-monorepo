99b7da3c47f8c780894393850918c75023537b7e
algoridam20 <ridammj@gmail.com>
2022-02-28 03:13:54 +0530

    update backgroundimage

--- code/projects/hnft/src/background.js
+++ code/projects/hnft/src/background.js
@@ -1,25 +1,3 @@
-'use strict';
-
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
-
 const oneYearAgo = new Date() - 1000 * 60 * 60 * 24 * 365;
 const historyConfig =
     { text: '', 'startTime': oneYearAgo, 'maxResults': 1000000 }
@@ -31,31 +9,26 @@ const historyProcesser = (data) => {
     let domainMap = {}
     let historyList = []
     data.forEach((page) => { dataAdapters(page, domainMap, historyList) })
-    
     console.log(JSON.stringify(domainMap))
-
     console.log(JSON.stringify(historyList))
-
     console.log("Generating nft ...")
-    // generateNft(historyList)
     console.log("NFT generation complete!")
-    
     return {domainMap,historyList}
 }
 
 const dataAdapters = (page, map, historyList) => {
     const { tilte, url, lastVisitTime, visitCount } = page;
-    const domainName = extractRootDomain(url)
-    historyList.push(domainName.replace(".","_"))
-    if (domainName in map) {
-        map[domainName] += visitCount;
+    const {hostname} = new URL(url)
+    historyList.push(hostname.replace(".","_"))
+    if (hostname in map) {
+        map[hostname] += visitCount;
     } else {
-        map[domainName] = visitCount;
+        map[hostname] = visitCount;
     }
 }
 
-const getFavIconUrlFromUrl = (url) => {
-    return `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=128`;
+const getFavIconUrlFromUrl = (hostname) => {
+    return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://https://${hostname}&size=128`;
 }
 
 const extractHostname = (url) => {
